34163f1f5de2f87208a1fe38dc711e8e8f8fdb01
Emily Wang <eyw23@cornell.edu>
2022-10-07 09:53:25 -0400

    Created pieces.mli
    - Wrote a general outline of the mli for pieces

---
+++ src/pieces.mli
@@ -0,0 +1,16 @@
+(** The chess pieces and their respective moves*)
+
+type name = string
+(** The type [name] is the name of the piece*)
+
+type value = int
+(** The type [value] represents the value of the piece*)
+
+type command =
+  | Go of string
+
+exception Invalid
+(** Raised when the move is invalid*)
+
+type moves = string list
+(** The type [moves] is a string list that lists the valid types of moves the piece can make *)
