077c5de7fc8be0717788fffa79f55f41e846c48f
Atulya Lohani <al878@cornell.edu>
2022-10-15 15:25:35 -0400

    Started implementing pawn

--- src/pieces.ml
+++ src/pieces.ml
@@ -7,6 +7,11 @@ type name = string
 type value = int
 (** The type [value] represents the value of the piece*)
 
+type loc = (int*int) 
+(** The type [loc] represents the location of the piece*)
+type captured =  bool
+(** The type[captured] represents whether this piece is captured or not*)
+
 type command =
   | Go of string
 
@@ -17,4 +22,14 @@ type moves = string list
 (** The type [moves] is a string list that lists the valid types of moves the piece can make *)
 
 end
+module Pawn : PIECES = struct
+let name = "Pawn"
+let value = 1
+let captured = false
 
+exception Invalid
+
+
+
+ 
+end
