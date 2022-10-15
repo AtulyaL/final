9de2e58877c550bdf966ecaaefd89766cbea2080
Jock Li <jl2698@cornell.edu>
2022-10-15 18:04:17 -0400

    Added board.mli
    -Added the board type for board.mli

--- src/board.mli
+++ src/board.mli
@@ -1,1 +1,10 @@
+open Pieces
+open Game
+
 (** Initiates the chess board and maintains it as the two players play*)
+
+type board = Node of (int * int)
+(** The type of the board, which is represented by a series of a node with tuple
+  of two ints*)
+
+(** *)
