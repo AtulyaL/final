7f0c715228ec9c7d7b1ce534831a8a2f1978399a
Emily Wang <eyw23@cornell.edu>
2022-10-15 16:01:00 -0400

    Updated pieces.ml

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -4,16 +4,23 @@ open Game
 open Pieces
 open Board
 
+let singleplayer = raise (Failure "Unimplemented")
+let multiplayer = raise (Failure "Unimplemented")
+
+let rec choose_gamemode =
+  print_endline "Please choose a gamemode: singleplayer or multiplayer:\n";
+  print_string "> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | "singleplayer" -> singleplayer
+  | "multiplayer" -> multiplayer
+  | _ -> choose_gamemode
+
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
   ANSITerminal.print_string [ ANSITerminal.red ]
     "\n\nWelcome to our chess command line interface!\n";
-  print_endline "Please choose a color (white or black):\n";
-  print_string "> ";
-  match read_line () with
-  | exception End_of_file -> ()
-  | "white" -> play_white
-  | "black" -> play_black
+  choose_gamemode
 
 (* Execute the game engine. *)
 let () = main ()
--- src/pieces.ml
+++ src/pieces.ml
@@ -1,40 +1,28 @@
-exception Invalid of string
+module type PIECE = sig
 
-<<<<<<< Updated upstream
-type name = string
-(** The type [name] is the name of the piece*)
-
-type value = int
-(** The type [value] represents the value of the piece*)
-
-type loc = (int*int) 
-(** The type [loc] represents the location of the piece*)
-type captured =  bool
-(** The type[captured] represents whether this piece is captured or not*)
-
-type command =
-  | Go of string
-
-exception Invalid
-(** Raised when the move is invalid*)
+type t = {name : string; value: int ; loc : (string * int) ; captured: bool; moves: string list; color: string}
+(** The type that represents the piece. [name] is the name of the piece. [value] is the amount of points the piece is worth
+    [loc] is the location of the piece. [captured] is whether the piece is captured or not *)
 
 type moves = string list
-(** The type [moves] is a string list that lists the valid types of moves the piece can make *)
+(** The type [moves] is a string list that lists the valid types of moves the piece can make *) 
 
+val name
+(** Returns the name of the piece*)
+
+val value 
+(** Returns the value of the piece*)
+end
+
+module Pawn : PIECE = struct
+
+  let t = {name = "pawn"; value = 1; loc = (string, 1); captured = false}
+
+  let moves = []
 end
 
 
-module Pawn : PIECES = struct
-let name = "Pawn"
-let value = 1
-let captured = false
-
-exception Invalid
-
-
-
+module Bishop : PIECE = struct
+  let t = {name = ""}
+end
  
-end
-=======
-type pawn = {name = "pawn"; value = 1 ; loc = (1,1)  ; captured = false}
->>>>>>> Stashed changes
