3cf331f059b423554bb0bb310a606131f69e99bf
Jock Li <jl2698@cornell.edu>
2022-10-16 12:21:58 -0400

    Formated pieces.ml
    -Added ocamlformat to the file

--- src/pieces.ml
+++ src/pieces.ml
@@ -1,24 +1,32 @@
 type t = {
-  name : string; 
-  value: int ; 
-  loc : (int * int) ; 
-  captured : bool; 
-  moves : (int * int) list; 
+  name : string;
+  value : int;
+  loc : int * int;
+  captured : bool;
+  moves : (int * int) list;
   color : string;
-  moved : bool
+  moved : bool;
 }
 
-let init_val name = match name with 
-| "pawn" -> 1
-| "knight" -> 3
-| "bishop" -> 3
-| "rook" -> 5
-| "queen" -> 9
-| "king" -> 99
-| _ -> int_of_float infinity
+let init_val name =
+  match name with
+  | "pawn" -> 1
+  | "knight" -> 3
+  | "bishop" -> 3
+  | "rook" -> 5
+  | "queen" -> 9
+  | "king" -> 99
+  | _ -> int_of_float infinity
 
-let init name color loc : t = {name = name; value = name |> init_val ; loc = loc;
- captured = false; color = color; moves=[]; moved = false}
+let init name color loc : t =
+  {
+    name;
+    value = name |> init_val;
+    loc;
+    captured = false;
+    color;
+    moves = [];
+    moved = false;
+  }
 
-let move ( t : t ) ( new_loc : (int * int ) ) =  { t with loc = new_loc ; moved=true}
-
+let move (t : t) (new_loc : int * int) = { t with loc = new_loc; moved = true }
