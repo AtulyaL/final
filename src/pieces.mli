452b9b33c00a1b5135646cd3a9a4756651b42c4b
Jock Li <jl2698@cornell.edu>
2022-10-16 19:34:55 -0400

    Edited pieces.mli

--- src/pieces.mli
+++ src/pieces.mli
@@ -1,12 +1,4 @@
-type t = {
-  name : string;
-  value : int;
-  loc : int * int;
-  captured : bool;
-  moves : (int * int) list;
-  color : string;
-  moved : bool;
-}
+type t
 (** Type t is the abstract type that represents a piece. A piece should have a
     name, value, current location, information on whether it's captured, a list
     of possible moves, color, and whether it has moved or not *)
