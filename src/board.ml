c61aa4cd0a030a91b58b681a5afe336a34ecf2a8
Daniel Lines <dal337@cornell.edu>
2022-10-16 13:44:31 -0400

    Cleaned up game.mli

--- src/board.ml
+++ src/board.ml
@@ -2,7 +2,7 @@ open Pieces
 open Game
  
 (** Initiates the chess board and maintains it as the two players play*)
- 
+
 type tile =
 | Empty of int * int
 | Full of int * int * Pieces
--- src/game.mli
+++ src/game.mli
@@ -1,21 +1,19 @@
-open Pieces
-
 type status
 (** type status is a record containing the status of the game; its fields are
     "white", "black", and "turn", which is whether or not each side is in check
     and whose turn it is*)
 
-val valid_move : board -> int * int -> t -> bool
+val valid_move : Board.board -> int * int -> t -> bool
 (**valid_move takes in a board, a move, and a piece and checks if it is a valid
    move based on the bool*)
 
-val check : board -> bool
+val check : Board.board -> bool
 (**check takes in a board and returns whether or not any side is in check*)
 
-val checkmate : board -> bool
+val checkmate : Board.board -> bool
 (**checkmate takes in a board and returns whether or not any side is in
    checkmate*)
 
-val update_status : status -> status
+val update_status : Board.status -> status
 (**update_status takes in the status of the game and returns the new status of
    the game*)
