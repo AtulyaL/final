c4e8f597b905c5f75bf3da3b5e0564b729961e74
Emily Wang <eyw23@cornell.edu>
2022-10-16 20:30:52 -0400

    Fixed logic.ml

--- src/logic.ml
+++ src/logic.ml
@@ -4,9 +4,10 @@ open Board
 (**first horizontal, then vertical for moves*)
 
 (** checks if pawn move is valid; returns a bool*)
-let pawn_move move info board = match move, info.loc with 
-| (u1, u2), (t1, t2) -> if u1 = t1 then (if u2 = t2 + 1 then is_occupied board move else if u2 = t2 + 2 then is_occupied board move && not info.moved) else
-  if (u1 = t1 + 1 || u1 = t1 - 1) && u2 = t2 + 1 then 
+let pawn_move move info board color = match move, info.loc with 
+| (u1, u2), (t1, t2) -> if u1 = t1 then (if u2 = t2 + 1 then (valid_move board move color) else if u2 = t2 + 2 then (valid_move board move color && not info.moved) else false) 
+else if (u1 = t1 + 1 || u1 = t1 - 1) && u2 = t2 + 1 then (valid_move board move color)
+else false
 
 let knight_move move info board = match move with 
 | (u1, u2) -> "x"
@@ -24,8 +25,8 @@ let king_move move info board = match move with
 | (u1, u2) -> "x"
 
 
-let check_move move info board = match info.name with 
-| "pawn" -> pawn_move move info board
+let check_move move info board color = match info.name with 
+| "pawn" -> pawn_move move info board color 
 | "knight" -> knight_move move info board
 | "rook" -> rook_move move info board
 | "bishop" -> bishop_move move info board
