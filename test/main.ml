9e5cacb1c1ccdef388b3f862d050816e330c379d
Jock Li <jl2698@cornell.edu>
2022-10-19 19:27:42 -0400

    Added lists for test cases

--- test/main.ml
+++ test/main.ml
@@ -3,4 +3,13 @@ open Game
 open Pieces
 open Board
 open Logic
-(* Test cases*)
+
+let board_tests = []
+let logic_tests = []
+let pieces_tests = []
+
+let suite =
+  "test suite for Chess Game"
+  >::: List.flatten [ board_tests; logic_tests; pieces_tests ]
+
+let _ = run_test_tt_main suite
