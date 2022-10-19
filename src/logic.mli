76e7f1e03fce4f4b5d80691887a3db8bcc2e4990
Daniel Lines <dal337@cornell.edu>
2022-10-19 19:33:30 -0400

    Changed logic.mli

--- src/logic.mli
+++ src/logic.mli
@@ -3,10 +3,6 @@ type status
     "white", "black", and "turn", which is whether or not each side is in check
     and whose turn it is*)
 
-val valid_move : Board.board -> int * int -> Pieces.t -> bool
-(**valid_move takes in a board, a move, and a piece and checks if it is a valid
-   move based on the bool*)
-
 val check : Board.board -> bool
 (**check takes in a board and returns whether or not any side is in check*)
 
