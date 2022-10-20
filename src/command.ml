44f5a322006a3968cd35ae8c57b9e90849d7a89a
Emily Wang <eyw23@cornell.edu>
2022-10-20 19:48:35 -0400

    Updated command

--- src/command.ml
+++ src/command.ml
@@ -1,3 +1,6 @@
+open Game
+open Board
+
 type object_phrase = {
   piece : string;
   move : int * int;
@@ -13,12 +16,14 @@ let parse_helper lst =
   | [ u1; u2 ] -> (int_of_string u1, int_of_string u2)
   | _ -> raise Invalid
 
-let parse str =
+let parse str board =
   let x = String.split_on_char ' ' str in
   let lst = List.filter (fun y -> y <> "") x in
   match lst with
   | [] -> raise Invalid
-  | u1 :: u2 :: u3 :: u4 :: u5 -> raise Invalid
+  | [ u1; u2; u3; u4 ] ->
+      let piece = find_piece (int_of_string u1, int_of_string u2) board in
+      { piece; move = (int_of_string u3, int_of_string u4) }
   | _ -> raise Invalid
 
 let move command = command.move
