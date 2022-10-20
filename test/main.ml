36032cac27b1fbda6a8be097effe47625e95909d
Jock Li <jl2698@cornell.edu>
2022-10-19 20:08:21 -0400

    Added helper function for test cases

--- test/main.ml
+++ test/main.ml
@@ -4,6 +4,11 @@ open Pieces
 open Board
 open Logic
 
+(** [init name] constructs an OUnit test in [board_tests] that asserts the
+    quality of [expected_output] with [init]. *)
+let init_test (name : string) (expected_output : board) : test =
+  name >:: fun _ -> assert_equal expected_output init
+
 let board_tests = []
 let logic_tests = []
 let pieces_tests = []
