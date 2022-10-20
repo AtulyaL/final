d49293f5cc76821952e72535f27e2d9dfacd9025
Jock Li <jl2698@cornell.edu>
2022-10-20 18:30:56 -0400

    Edited docstring for logic.mli

--- src/logic.mli
+++ src/logic.mli
@@ -4,14 +4,16 @@ type status
     and whose turn it is*)
 
 val check_move : int * int -> Pieces.t -> Board.board -> string -> status
+(** check_move takes in a move, a piece, a board, and a color and then returns a
+    bool called status which checks if the piece can move*)
 
 val check : Board.board -> bool
-(**check takes in a board and returns whether or not any side is in check*)
+(** check takes in a board and returns whether or not any side is in check*)
 
 val checkmate : Board.board -> bool
-(**checkmate takes in a board and returns whether or not any side is in
-   checkmate*)
+(** checkmate takes in a board and returns whether or not any side is in
+    checkmate*)
 
 val update_status : status -> status
-(**update_status takes in the status of the game and returns the new status of
-   the game*)
+(** update_status takes in the status of the game and returns the new status of
+    the game*)
